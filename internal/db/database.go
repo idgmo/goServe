9c9d61d2d2a5990c4030c7c6f527a05e5c1cc8a6
idgmo <ethan.a.mccoy@gmail.com>
2021-08-11 14:32:18 -0400

    mysql branch

--- internal/db/database.go
+++ internal/db/database.go
@@ -13,14 +13,14 @@ import (
 )
 
 func CreateDatabase() (*sql.DB, error) {
-	// serverName := "localhost:3306"
+	serverName := "localhost:3306"
 	user := "myuser"
 	password := "pw"
 	dbName := "demo"
 
-	// connectionString := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&collation=utf8mb4_unicode_ci&parseTime=true&multiStatements=true", user, password, serverName, dbName)
-	cString := fmt.Sprintf("%s:%s@/%s", user, password, dbName)
-	db, err := sql.Open("mysql", cString)
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&collation=utf8mb4_unicode_ci&parseTime=true&multiStatements=true", user, password, serverName, dbName)
+
+	db, err := sql.Open("mysql", connectionString)
 	if err != nil {
 		return nil, err
 	}
